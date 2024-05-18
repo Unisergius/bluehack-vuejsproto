6b7c9b891fb6fc5f91e32ab54f1d2c2984f77ed7
Sergio GUERREIRO-HENRIQUES <sergio.guerreiro-henriques@vinci-energies.net>
2024-05-18 20:47:12 +0100

    Countdown implementation

--- bluehack-proto/tailwind.config.js
+++ bluehack-proto/tailwind.config.js
@@ -6,7 +6,7 @@ export default {
     },
     plugins: [require("daisyui")],
     daisyui: {
-        themes: ["corporate", "synthwave", "cmyk"],
+        themes: ["corporate", "synthwave"],
     },
 }
 
