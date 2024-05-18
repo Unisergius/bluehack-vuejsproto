ea7b7217c0d151ca168e6809e4e7f8babeb1c6f7
Sergio GUERREIRO-HENRIQUES <sergio.guerreiro-henriques@vinci-energies.net>
2024-05-18 19:36:09 +0100

    Partners fixes

--- bluehack-proto/src/components/PartnerCard.vue
+++ bluehack-proto/src/components/PartnerCard.vue
@@ -1,42 +1,43 @@
 <!-- src/components/PartnerCard.vue -->
 <template>
-    <div class="card w-96 glass m-4">
-      <figure>
-        <img :src="partner.image" :alt="partner.name" />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">{{ partner.name }}</h2>
-        <p>{{ partner.description }}</p>
-        <div class="card-actions justify-end">
-          <a v-if="partner.website" :href="partner.website" class="text text-primary" target="_blank">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" fill="currentColor"  viewBox="0 -960 960 960" width="24px">
-                <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
-            </svg>
-          </a>
-          <a v-if="partner.linkedin" :href="partner.linkedin" target="_blank" class="text text-primary">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M4.98 3.5C4.98 2.12 6.11 1 7.5 1S10 2.12 10 3.5 8.87 6 7.5 6 4.98 4.88 4.98 3.5zM3 8.5h9v12h-9zM22 13.72V20.5h-4v-5.68c0-3.11-4-2.87-4 0v5.68h-4v-12h4v1.79c1.76-2.97 8-3.22 8 2.86z"/>
-            </svg>
-          </a>
-          <!-- Add more social media icons as needed -->
+    <div class="card w-96 p-4  shadow-xl">
+        <figure class="bg-base-600">
+            <img :src="partner.image" :alt="partner.name" class="h-48 object-contain" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">{{ partner.name }}</h2>
+            <p>{{ partner.description }}</p>
+            <div class="card-actions justify-end">
+            <a v-if="partner.website" :href="partner.website" class="text text-primary" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" fill="currentColor"  viewBox="0 -960 960 960" width="24px">
+                    <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
+                </svg>
+            </a>
+            <a v-if="partner.linkedin" :href="partner.linkedin" target="_blank" class="text text-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M4.98 3.5C4.98 2.12 6.11 1 7.5 1S10 2.12 10 3.5 8.87 6 7.5 6 4.98 4.88 4.98 3.5zM3 8.5h9v12h-9zM22 13.72V20.5h-4v-5.68c0-3.11-4-2.87-4 0v5.68h-4v-12h4v1.79c1.76-2.97 8-3.22 8 2.86z"/>
+                </svg>
+            </a>
+            <!-- Add more social media icons as needed -->
+            </div>
         </div>
-      </div>
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
+<script>
+
+export default {
     name: 'PartnerCard',
     props: {
-      partner: {
-        type: Object,
-        required: true
-      }
-    }
-  };
-  </script>
-  
-  <style scoped>
-  /* Add any custom styles here */
-  </style>
+        partner: {
+            type: Object,
+            required: true
+        }
+    },
+};
+</script>
+
+<style scoped>
+/* Add any custom styles here */
+</style>
   
--- bluehack-proto/src/pages/Partners.vue
+++ bluehack-proto/src/pages/Partners.vue
@@ -1,45 +1,89 @@
 <template>
-    <div class="flex flex-wrap justify-center">
-      <PartnerCard
-        v-for="(partner, index) in partners"
-        :key="index"
-        :partner="partner"
-      />
+    <div class="hero h-96 bg-base-200">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">Partners</h1>
+                <p class="py-6">Our partners drive innovation and excellence, providing vital support and resources to advance marine science and technology solutions.</p>
+            </div>
+        </div>
+    </div>
+    <div class="container mx-auto p-6">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <PartnerCard
+                v-for="(partner, index) in partners"
+                :key="index"
+                :partner="partner"
+            />
+        </div>
     </div>
   </template>
   
-  <script>
-  import PartnerCard from '@/components/PartnerCard.vue';
-  
-  export default {
+<script>
+import PartnerCard from '@/components/PartnerCard.vue';
+
+import backgroundUrl from '@/assets/images/maksym-kaharlytskyi-QQcLNBQ6i_A-unsplash.jpg';
+import ccmarImage from '@/assets/images/ccmar.png';
+import criaImage from '@/assets/images/cria.jpeg';
+import greenColabImage from '@/assets/images/green-colab-cores.png';
+import ualgTecCampusImage from '@/assets/images/ualg_tec_campus_logo.jpeg';
+import localFoundationImage from '@/assets/images/local_foundation_logo.jpeg';
+import geekSessionsImage from '@/assets/images/GS_CORES_FUNDO squared.png';
+
+export default {
     components: {
-      PartnerCard
+        PartnerCard
     },
     data() {
-      return {
-        partners: [
-          {
-            name: 'Life hack',
-            description: 'How to park your car at your garage?',
-            image: 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg',
-            website: 'https://example.com',
-            linkedin: 'https://linkedin.com/in/example'
-          },
-          {
-            name: 'Another Partner',
-            description: 'Description for another partner.',
-            image: 'https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg',
-            website: 'https://anotherexample.com',
-            linkedin: 'https://linkedin.com/in/anotherexample'
-          }
-          // Add more partner objects as needed
-        ]
-      };
+        return {
+            backgroundUrl,
+            partners: [
+                {
+                    name: 'CCMAR Algarve',
+                    description: 'Non-profit organization located on the Gambelas campus of the University of Algarve and dedicated to R&D in the Marine Sciences. ',
+                    image: ccmarImage,
+                    website: 'https://ccmar.ualg.pt/',
+                    linkedin: 'https://www.linkedin.com/company/ccmar/mycompany/'
+                },
+                {
+                    name: 'CRIA',
+                    description: 'Division of Entrepreneurship and Technology Transfer of the University of Algarve.',
+                    image: criaImage,
+                    website: 'https://www.cria.pt/cria/presentation',
+                },
+                {
+                    name: 'GreenColab',
+                    description: 'Joining the Pieces in Algal Biotechnology<br>A collaborative platform to bridge the transfer of knowledge from academia, industry, stakeholders, and consumers.',
+                    image: greenColabImage,
+                    website: 'https://www.greencolab.com/',
+                    linkedin: 'https://www.linkedin.com/company/greencolab/'
+                },
+                {
+                    name: 'Algarve Tech Hub',
+                    description: 'Developing a robust ecosystem in the area of new technologies, attracting international stakeholders, technological talent, investors and digital nomads to the Algarve.',
+                    image: ualgTecCampusImage,
+                    website: 'https://algarvetechhub.com/',
+                    linkedin: 'https://www.linkedin.com/company/ualg-tec-campus/'
+                },
+                {
+                    name: 'local.foundation',
+                    description: 'Providing right tools and mindset to work together on building better communities.  ',
+                    image: localFoundationImage,
+                    linkedin: 'https://www.linkedin.com/company/local-foundation/'
+                },
+                {
+                    name: 'GeekSessions',
+                    description: 'Fostering knowledge exchange and collaboration within the Digital Product community of the Algarve (and beyond!).',
+                    image: geekSessionsImage,
+                    website: 'https://geeksessions.io/',
+                }
+            // Add more partner objects as needed
+            ]
+        };
     }
-  };
-  </script>
+};
+</script>
   
-  <style scoped>
-  /* Add any custom styles here */
-  </style>
+<style scoped>
+/* Add any custom styles here */
+</style>
   
