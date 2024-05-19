63440ad1477808f93287f072a3cd4ff7fbaab54b
palec87 <david@stanka.de>
2024-05-19 09:24:58 +0100

    navbar and about

--- bluehack-proto/src/components/Header.vue
+++ bluehack-proto/src/components/Header.vue
@@ -2,19 +2,18 @@
     <header class="shadow-md z-50">
         <div class="navbar bg-base-100">
             <div class="navbar-start">
-                <div class="dropdown">
-                    <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-                    </div>
-                    <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li v-for="link in links" :key="link.text">
-                            <RouterLink :to="link.to">{{ link.text }}</RouterLink>
-                        </li>
-                    </ul>
+                <div class="navbar-start">
+                    <a class="btn btn-ghost text-xl h-10" >
+                        <RouterLink to="/about"><img id="logo" class="h-6" src="@/assets/images/logo.png" /></RouterLink>
+                        </a>
                 </div>
             </div>
-            <div class="navbar-center">
-                <a class="btn btn-ghost text-xl h-10" ><img id="logo" class="h-6" src="@/assets/images/logo.png" /></a>
+            <div class="navbar-center hidden lg:flex">
+                <ul class="menu menu-horizontal px-2 rounded-box">
+                    <li v-for="link in links" :key="link.text">
+                        <RouterLink :to="link.to">{{ link.text }}</RouterLink>
+                    </li>
+                </ul>
             </div>
             <div class="navbar-end">
                 <ThemeToggle></ThemeToggle>
@@ -34,7 +33,7 @@
             return {
                 links: [
                     { to: '/', text: 'Home' },
-                    { to: '/about', text: 'About Us' },
+                    // { to: '/about', text: 'About Us' },
                     { to: '/eventInfo', text: 'Event Info' },
                     { to: '/contact', text: 'Contact' },
                     { to: '/partners', text: 'Partners' }
--- bluehack-proto/src/pages/AboutUs.vue
+++ bluehack-proto/src/pages/AboutUs.vue
@@ -1,14 +1,51 @@
 <template>
-    <div>
-        <h1>About</h1>
-        <p>This is the About page.</p>
+    <div class="hero " :style="{ backgroundImage: `url(${backgroundUrl})` }">
+        <div class="hero-overlay bg-opacity-60">
+        </div>
+        <div class="hero-content text-left">
+            <div class="container mx-auto p-6">
+            <!-- Philosophy -->
+            <section class="bg-base-200 p-6 rounded-lg shadow-lg mb-6">
+                <h2 class="text-3xl font-bold mb-4">Our philosophy</h2>
+                <p class="mb-4">
+                    <em>Code Blue</em> takes prides in tackling multifacet and complicated challenges
+                    of the current world, which need contributions from as many angles as possible.
+                    We believe everyone knows something we could use towards humanity progress. We focus on science, knowledge transfer,
+                    sustainability, and innovation.
+                </p>
+            </section>
+
+            <!-- Our Team -->
+            <section class="bg-base-200 p-6 rounded-lg shadow-lg mb-6">
+                <h2 class="text-3xl font-bold mb-4">Our Team</h2>
+                <p class="mb-4">
+                    Teresa Correia
+                </p>
+                <p class="mb-4">
+                    Lenz Gschwendtner
+                </p>
+                <p class="mb-4">
+                    Sergio Henriques
+                </p>
+                <p class="mb-4">
+                    David Paleček
+                </p>
+            </section>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'AboutUs'
-}
+    import backgroundUrl from '@/assets/images/maksym-kaharlytskyi-QQcLNBQ6i_A-unsplash.jpg';
+    export default {
+        name: 'AboutUs',
+        data() {
+            return {
+                backgroundUrl,
+            };
+        },
+    }
 </script>
 
 <style scoped>
