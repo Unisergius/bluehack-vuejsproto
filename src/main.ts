bfbbb502653a4efdc3e58462a814d307252dbd00
palec87 <david@stanka.de>
2024-09-18 19:20:16 +0100

    first pt setup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import './style.css'
 import './index.css'
 import App from './App.vue'
 import router from './router/index.ts'
 
-createApp(App).use(router).mount('#app');
+// Import translation files
+import en from './i18n/en.json'
+import pt from './i18n/pt.json'
+
+const i18n = createI18n({
+    // something vue-i18n options here ...
+    locale: 'en', // set locale
+    fallbackLocale: 'en', // set fallback locale
+    messages: {
+        en,
+        pt
+    }
+  })
+
+createApp(App)
+    .use(router)
+    .use(i18n)
+    .mount('#app');
